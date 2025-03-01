a31ba1e2d7c2c1bbf9d82daeeb7128312013d0f2
hinata <hinatachikao1971@gmail.com>
2025-03-01 06:37:56 +0330

    change script file

--- git-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# grep -rli 'cpp_001' * | xargs -i@ sed -i 's/cpp_001/new_cpp_project/g' @
-git init 
-git config user.name "hinata" 
-git config user.email "hinatachikao1971@gmail.com" 
-git add --all 
-git commit -m 'first commit'
